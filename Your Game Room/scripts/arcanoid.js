1af861cb9e8e9a348894ed427bd0b82b579e9986
Arseniy Bikulov <arsenybikulov@gmail.com>
2022-10-07 15:08:10 +0700

    Анимация для кнопок "Арканоид"
    Добавлена анимация для кнопок сервиса на странице игры "Арканоид"

--- Your Game Room/scripts/arcanoid.js
+++ Your Game Room/scripts/arcanoid.js
@@ -334,3 +334,21 @@ function renderBricks(){
 		c.restore();	
 	}
 }
+
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("control-but");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
